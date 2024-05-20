9091952515f7069408a5dd45afa79c7fca82b03e
TCNJDIGITall <145065603+TCNJDIGITall@users.noreply.github.com>
2024-05-20 14:25:03 -0400

    Update members.js

--- scripts/members.js
+++ scripts/members.js
@@ -1,5 +1,8 @@
+// this script file is present only on the members page of the application.
+
 var content = document.getElementById("content")
 
+// condition that runs if user is not on a mobile device
 if(!(/Mobi|Android/i.test(navigator.userAgent))){
     if(scale >= 1.5){
         content.style.scale = "1.075"
